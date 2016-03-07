a624adca10cd70723d4e3bc57591222d57ae6256
Alexey Bondarenko <alexeybondarenko@me.com>
2016-03-07 12:08:52 +0200

    error codes and disconnect event if rerror status is 100

--- www/bg1.js
+++ www/bg1.js
@@ -4,9 +4,7 @@ cordova = require('cordova');
 
 var iHealth = function () {
   this.channels = {
-    ihealthstatus: cordova.addWindowEventHandler("ihealthstatus"),
-    ihealthconnect: cordova.addWindowEventHandler("ihealthconnect"),
-    ihealthdisconnect: cordova.addWindowEventHandler("ihealthdisconnect"),
+    ihealthstatus: cordova.addWindowEventHandler("ihealthstatus")
   };
   for (var key in this.channels) {
     this.channels[key].onHasSubscribersChange = onHasSubscribersChange;
@@ -31,24 +29,45 @@ function onHasSubscribersChange () {
 iHealth.prototype._status = function (info) {
   console.log('status', JSON.stringify(info));
   if (info) {
-    switch (info.name) {
-      case 'connect': {
-        cordova.fireWindowEvent("ihealthconnect", info);
+
+    switch (info.status) {
+      case 'error': {
+        if (!info.data) break;
+        if (info.data.status == 100) {
+          info.name = 'disconnect';
+        }
+
         break;
-      }
-      case 'disconnect': {
-        cordova.fireWindowEvent("ihealthdisconnect", info);
-        break;
-      }
-      default: {
-        cordova.fireWindowEvent("ihealthstatus", info);
       }
     }
+    cordova.fireWindowEvent("ihealthstatus", info);
   }
 };
 
 iHealth.prototype._error = function (e) {
   console.log("Error initializing iHealth: " + e);
+};
+
+iHealth.prototype.ERROR_CODES = {
+  "00": "Battery is low.",
+  "01": "Glucose test result is out of the measurement range.",
+  "02": "Unknown interference detected, please repeat the test.",
+  "03": "Strip is used or unknown moisture detected, discard the test strip and repeat the test with a new strip.",
+  "04": "Reading transmission error. Repeat the test with a new test strip. \n If the problem persists, contact iHealth customer service for assistance.",
+  "05": "The environmental temperature is beyond normal range, place the meter at room temperature for at least 30 minutes, then repeat the test.",
+  "06": "The environmental temperature is beyond normal range, place the meter at room temperature for at least 30 minutes, then repeat the test.",
+  "07": "Test strip coding error.",
+  "08": "Communication error, press“START” or rescan the code to repeat the test.",
+  "09": "Strip removed in the middle of reading, repeat the test with a new strip.",
+  "10": "Insert a new test strip and repeat the test.",
+  "11": "Cannot write to SN or KEY.",
+  "12": "Please set time.",
+  "13": "0 test strips remaining.",
+  "14": "Test strip expired.",
+  "15": "Unplug the charging cable before testing.",
+  "100": "BG meter disconnected.",
+  "101": "BG meter is in sleeping mode, needs repair.",
+  "111": "user verification failed."
 };
 
 var ihealth = new iHealth();
